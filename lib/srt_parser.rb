144fc1c3a2490e6a250e5a95b7190840584b0326
DarryQueen <slimfrog@gmail.com>
2015-04-01 23:44:46 -0700

    Added merge functionality for multiple subtitles for presumably same video.

--- lib/srt_parser.rb
+++ lib/srt_parser.rb
@@ -26,6 +26,14 @@ module SRTParser
       @intervals = intervals
     end
 
+    # Accessors:
+    def duration
+      @duration
+    end
+    def intervals
+      @intervals
+    end
+
     def coverage
       covered = 0
       @intervals.each do |interval|
@@ -33,6 +41,27 @@ module SRTParser
       end
 
       covered / @duration
+    end
+
+    # Merge lists of intervals. Going to this in a very disgusting way:
+    def self.merge(*subtitles)
+      intervals = subtitles.map { |subtitle| subtitle.intervals }
+        .inject([]) { |all, intervals| all.concat(intervals) }
+        .sort_by { |interval| interval[0] }
+
+      # Start merging the intervals. Totally not DRY:
+      times = []
+
+      intervals.each do |interval|
+        if times.last and interval[0] <= times.last[1]
+          times.last[1] = [times.last[1], interval[1]].max
+        else
+          times.push(Array.new(interval))
+        end
+      end
+
+      durations = subtitles.map { |subtitle| subtitle.duration }
+      Subtitle.new(durations.max, times)
     end
   end
 end
